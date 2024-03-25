ce9cc4bbc46fdc770560d5d78fe30104b623791c
Andi Mataraja <INT00857@mylab.local>
2024-03-25 13:46:24 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,72 @@
-
-
 <template>
   <h1>{{ title }}</h1>
-  <br>
-  <input type="text" ref="name">
+  <br />
+  <input type="text" ref="name" />
   <button @click="handleClick">Click Me</button>
-  <Modal header="Sign Up for the Giveaway!"/>
+
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="dark" @close="toggleModal"> <!-- ada 2 theme dark dan sale -->
+      <h1>Hello World</h1>
+      <p>Grab your stuff for half price!</p>
+      <template v-slot:links>
+        <a href="">Sign Up Now</a> 
+        <a href="">More info</a>
+      </template>
+    </Modal>
+  </teleport>
+
+  <br />
+
+  <teleport to="#modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <h1>World Hello</h1>
+      <p>Blablabla Blablabla</p>
+      <template v-slot:links>
+        <a href="">Click Me 1</a>
+        <a href="">Click Me 2</a>
+      </template>
+    </Modal>
+  </teleport>
+
+  <br />
+
+  <button @click="toggleModal2">Open Second Modal</button>
+  <button @click.alt="toggleModal">Open Modal (alt)</button>
 </template>
 
-
 <script>
-import Modal from './components/Modal.vue'
-
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {Modal},
-  data(){
-    return{
-      title: 'My First Vue App :)'
-    }
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My First Vue App :)",
+      header: "Sign Up for the Giveaway!",
+      text: "Grab your stuff for half price!",
+      showModal: false,
+      showModal2: false
+    };
   },
   methods: {
-    handleClick(){
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')  //adding class to ref
-      this.$refs.name.focus() //ref will be focused
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active"); //adding class to ref
+      this.$refs.name.focus(); //ref will be focused
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2(){
+      this.showModal2 = !this.showModal2;
     }
   }
-}
+};
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -41,10 +75,13 @@ export default {
   margin-top: 60px;
 }
 
-h1{
+h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
 }
 
+button{
+  margin: 10px;
+}
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,48 +1,96 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-      <h1>{{ header }}</h1>
-      <p>modal content</p>
+  <div class="backdrop" @click.self="closeModal">
+    <div
+      class="modal"
+      :class="{ sale: theme === 'sale', dark: theme === 'dark' }"
+    >
+      <!-- <h1>{{ header }}</h1>
+      <p>{{ text }} </p> -->
+      <slot> </slot> <!-- similar like yield and section in laravel --> 
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default{
-    props: [
-      'header'
-    ]
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    }
   }
+};
 </script>
 
 <style>
-  .modal{
-    width: 400px;
-    padding: 20px;
-    margin: 100px auto;
-    background: white;
-    border-radius: 10px;
-  }
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
 
-  .backdrop{
-    top: 0;
-    position: fixed;
-    background: rgba(0, 0, 0, 0.5);
-    width: 100%;
-    height: 100%;
-  }
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
 
-  .modal h1{
-    color: #03cfb4;
-    border: none;
-    padding: 0;
-  }
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
 
-  /* 
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
+}
+
+.modal.sale a{
+  color: purple;
+}
+
+.modal.dark {
+  background: darkslategrey;
+  color: white;
+}
+
+.modal.dark a{
+  color: coral;
+}
+
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+  text-decoration: none;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  margin: 10px
+}
+
+.modal .actions a:hover{
+  text-decoration: underline;
+}
+
+/* 
     There are 2 ways for styling in vue:
     1. Scoped Style -> the css only works for 1 vue file
     2. .[***.vue] [tag] -> the css only affect for the tag in 1 file.
     3. Without scoped -> css global
   */
-
 </style>
